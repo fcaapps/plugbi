ad66e8926218e357ba96ee38efd42d2bf6df0362
fcaapps <fca.apps@gmail.com>
2018-10-27 13:26:08 -0300

    Usuário Model Criado - Conf

--- contas/models.py
+++ contas/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class User(AbstractUser):
+
+    username = models.CharField(
+        error_messages={'unique': 'A user with that username already exists.'},
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        max_length=150,
+        validators=[UnicodeUsernameValidator()],
+        verbose_name='Nome'
+    )
 
     email = models.EmailField(
         verbose_name='E-mail',
         max_length=255,
         unique=True,
     )
-
 
 
     USERNAME_FIELD = 'email'
