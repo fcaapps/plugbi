24f62ebad5f089a2c134540abc7d6e905d959933
fcaapps <fca.apps@gmail.com>
2018-11-17 16:26:10 -0300

    Incluída página de política de privacidade

--- modelos/urls.py
+++ modelos/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import modelos
+from .views import modelos, politica
 
 urlpatterns = [
     path('', modelos, name="modelos"),
+    path('politica-privacidade', politica, name="politica"),
 ]
--- modelos/views.py
+++ modelos/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def modelos(request):
     return render(request, 'modelos.html')
+
+def politica(request):
+    return render(request, 'politica.html')
